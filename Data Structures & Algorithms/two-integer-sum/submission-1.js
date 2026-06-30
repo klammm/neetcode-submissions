4be00647427ac649540f8d0cf398719004c906af
Kevin Lam <kevinlam.sf@gmail.com>
2026-06-30 16:03:33 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        /*
+        Create a hash where the keys are the values nums[i] and the value is the index of the nums element i
+        Iterate nums
+        If target - nums[i] exists, then return the current index i and the hash[nums[i]] 
+        */
+
+        const hash = {};
+        const result = [];
+
+        for (let i = 0; i < nums.length; i++) {
+            if (hash[nums[i]]) {
+                hash[nums[i]] = i;
+            } else {
+                hash[nums[i]] = i;
+            }
+        }
+
+        for (let j = 0; j < nums.length; j++) {
+            const sub = target - nums[j];
+            if (hash[sub] && hash[sub] !== j) {
+                return [j, hash[sub]]
+            }
+        }
+
+        
+    }
+}
