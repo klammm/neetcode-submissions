2777cfd0af4738e624261e73a7a0b21b7b87237a
Kevin Lam <kevinlam.sf@gmail.com>
2026-08-05 12:09:41 -0700

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1, word2) {
+        let i = 0;
+        let j = 0;
+        let result = "";
+
+        while (i < word1.length || j < word2.length) {
+            if (word1[i]) {
+                result += word1[i];
+            }
+
+            if (word2[j]) {
+                result+= word2[j];
+            }
+
+            i++;
+            j++;
+        }
+
+        return result;
+    }
+}
